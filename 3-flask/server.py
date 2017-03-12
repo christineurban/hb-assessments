bb226d79cb57b6b2428c09c9b3d9b0565f9e18cc
Christine Urban <christineurbansf@gmail.com>
2017-03-12 04:55:23 +0000

    Completed functionality and formatted with Bootstrap

--- 3-flask/server.py
+++ 3-flask/server.py
@@ -4,12 +4,42 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
+JOBS = ["software engineer", "QA engineer", "product manager"]
+
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 
 
-# YOUR ROUTES GO HERE
+@app.route("/")
+def index():
+    """Return homepage."""
+
+    return render_template("index.html")
+
+
+@app.route("/application-form")
+def app_form():
+    """Return application page."""
+
+    return render_template("application-form.html",
+                           jobs=JOBS)
+
+
+@app.route("/application-success")
+def app_success():
+    """Return confirmation page."""
+
+    firstname = request.args.get("firstname")
+    lastname = request.args.get("lastname")
+    salary = request.args.get("salary")
+    jobtype = request.args.get("jobtype")
+
+    return render_template("application-response.html",
+                           firstname=firstname,
+                           lastname=lastname,
+                           salary=salary,
+                           jobtype=jobtype)
 
 
 if __name__ == "__main__":
